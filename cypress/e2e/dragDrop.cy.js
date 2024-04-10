ff7fb9c48adb78820ce6935026b81c33861f43f3
lyes <bob.lyes@live.com>
2024-04-10 23:11:45 +0200

    add assertio to check element that dropped

--- cypress/e2e/dragDrop.cy.js
+++ cypress/e2e/dragDrop.cy.js
@@ -1,8 +1,19 @@
 describe('Test suite drag and drop', () => {
-    it('should drag and drop', () => {
+    it('should drag and drop demoqa site', () => {
       cy.visit('https://demoqa.com/droppable')
-      cy.get('#draggable').drag('#droppable', {force:true})
+      cy.get('#draggable').drag('#droppable', {force:true}).then((success) => {
+        assert.isTrue(success)
+      })
       cy.contains('Dropped!').should('be.visible')
+     
+    })
+
+    xit('should drag and drop herokuapp site', () => {
+      cy.visit('https://the-internet.herokuapp.com/drag_and_drop')
+      cy.get('#column-a').trigger('mouseover').drag('#column-b', {force:true}).then((success) => {
+        assert.isTrue(success)
+      })
+      
      
     })
   })
